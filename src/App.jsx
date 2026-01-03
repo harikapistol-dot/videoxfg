b9ecfa4ea6b2c54049e8b7c1867b872008cd2392
harikapistol-dot <harikapistol@gmail.com>
2026-01-03 22:35:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { useWebRTC } from './hooks/useWebRTC';
+import Dashboard from './components/Dashboard';
+import VideoCall from './components/VideoCall';
+import Messenger from './components/Messenger';
+import Dialog from './components/Dialog';
+
+function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io('https://video-backend-51o4.onrender.com', { transports: ['websocket'] });
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  const {
+    localStream,
+    remoteStream,
+    personalCode,
+    messages,
+    inCall,
+    callType,
+    micActive,
+    cameraActive,
+    messagingReady,
+    dialog,
+    isMuted,
+    initiateCall,
+    acceptCall,
+    rejectCall,
+    hangUp,
+    sendMessage,
+    toggleMic,
+    toggleCamera,
+    rotateCamera,
+    toggleMute,
+    registerPersonalCode,
+    setDialog
+  } = useWebRTC(socket);
+
+  return (
+    <div className="main_container">
+      <Dashboard
+        personalCode={personalCode}
+        onInitiateCall={initiateCall}
+        onRegisterCode={registerPersonalCode}
+        disabled={inCall}
+      />
+
+      <VideoCall
+        localStream={localStream}
+        remoteStream={remoteStream}
+        inCall={inCall}
+        callType={callType}
+        micActive={micActive}
+        cameraActive={cameraActive}
+        isMuted={isMuted}
+        onToggleMic={toggleMic}
+        onToggleCamera={toggleCamera}
+        onRotateCamera={rotateCamera}
+        onToggleMute={toggleMute}
+        onHangUp={hangUp}
+      />
+
+      <Messenger
+        messages={messages}
+        onSendMessage={sendMessage}
+        inCall={inCall}
+        messagingReady={messagingReady}
+      />
+
+      <Dialog
+        dialog={dialog}
+        onAccept={acceptCall}
+        onReject={() => {
+          rejectCall();
+          setDialog(null);
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
